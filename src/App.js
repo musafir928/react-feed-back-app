6e960b648e02f7e3298d06a9b08eceeac639d7de
musafir928 <uganoghli@gmail.com>
2022-03-03 16:49:06 +0100

    pass prop via component and type check

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
+import Header from "./components/Header"
+
 const App = () => {
-    const students = [
-        {name: "adil", age: 35},
-        {name: "nurgul", age: 35},
-        {name: "asiye", age: 10},
-        {name: "maryam", age: 8},
-    ]
     return (
         <>
-        <ul>
-            {
-                students.map(s => {
-                   return <li key={s.name}>name: {s.name}  age: {s.age}</li> 
-                })
-            }
-        </ul>
+        <Header />
         </>
     )
 }
